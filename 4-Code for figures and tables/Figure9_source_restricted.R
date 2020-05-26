011eb28e4746fb159540c8343bd25f773b60b8d4
Helen Holah <h.f.holah@hotmail.co.uk>
2020-05-26 13:31:21 +0100

    Update Figure9_source_restricted.R
    Updated to include fishing footprint updates

--- 4-Code for figures and tables/Figure9_source_restricted.R
+++ 4-Code for figures and tables/Figure9_source_restricted.R
@@ -22,9 +22,12 @@ afteryear <- 2012:2019
 metier_mbcg  <- c("Otter","Beam","Dredge","Seine", 
                   "OT_CRU","OT_DMF","OT_MIX","OT_MIX_CRU_DMF",
                   "OT_MIX_DMF_BEN","OT_SPF")
-metier_static <- "Static"
+metier_static <- c("Static","Static_FPO","Static_GNS","Static_LLS") #HH
 
-nam <- c(paste("SAR_total",refyear,sep="_"),paste(metier_static[1],refyear, sep="_"))
+nam <- c(paste("SAR_total",refyear,sep="_"),paste(metier_static[2],refyear, sep="_"), #HH
+         paste(metier_static[3],refyear, sep="_")
+         ,paste(metier_static[4],refyear, sep="_"))
+
 indexcol <- which(names(vmsreg) %in% nam) 
 vmsreg$ref <- rowSums(vmsreg[indexcol])
 vmsreg$ref[vmsreg$ref > 0] <- 1
@@ -33,6 +36,33 @@ vmsreg$ref[vmsreg$ref > 0] <- 1
 IREG <- cbind(IREG, vmsreg[match(IREG$csquares,vmsreg$c_square), c("ref")])
 colnames(IREG)[ncol(IREG)] <- "ref"
 IREG$ref[is.na(IREG$ref)] <- 0
+
+# calculate adjacent squares #HH
+dat <- IREG[IREG$ref==1,]
+dat$long <- round(dat$long,3)
+dat$lat <- round(dat$lat,3)
+lonlats <- paste(dat$long, dat$lat, sep=":")
+
+for(i in (1:dim(dat)[1])){
+  
+  nw  <- paste(dat$long[i]-0.05, dat$lat[i]+0.05, sep=":")
+  nn  <- paste(dat$long[i]+0.00, dat$lat[i]+0.05, sep=":")
+  ne  <- paste(dat$long[i]+0.05, dat$lat[i]+0.05, sep=":")
+  ee  <- paste(dat$long[i]+0.05, dat$lat[i]+0.00, sep=":")
+  se  <- paste(dat$long[i]+0.05, dat$lat[i]-0.05, sep=":")
+  ss  <- paste(dat$long[i]+0.00, dat$lat[i]-0.05, sep=":")
+  sw  <- paste(dat$long[i]-0.05, dat$lat[i]-0.05, sep=":")
+  ww  <- paste(dat$long[i]-0.05, dat$lat[i]+0.00, sep=":")
+  
+  dat$adjacent.cells[i] <- sum(nw %in% lonlats[-i], nn %in% lonlats[-i], ne %in% lonlats[-i],
+                               ee %in% lonlats[-i], se %in% lonlats[-i], ss %in% lonlats[-i],
+                               sw %in% lonlats[-i], ww %in% lonlats[-i])
+}
+table(dat$adjacent.cells)
+
+IREG <- merge(x = IREG, y = dat[ , c("csquares", "adjacent.cells")], by = "csquares", all.x=TRUE)
+IREG$adjacent.cells[is.na(IREG$adjacent.cells)] <- 0
+
 
 # get vme data
 VME <- read.csv(paste(pathdir_nogit,
