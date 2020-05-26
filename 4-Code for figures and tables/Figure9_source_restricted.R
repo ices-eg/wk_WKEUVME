7dce9620bec70ab91fe3735ea89329c61e795234
Helen Holah <h.f.holah@hotmail.co.uk>
2020-05-26 14:04:25 +0100

    Update Figure9_source_restricted.R
    subset fig9 for fishing footprint scenario

--- 4-Code for figures and tables/Figure9_source_restricted.R
+++ 4-Code for figures and tables/Figure9_source_restricted.R
@@ -83,7 +83,9 @@ VMEVMS$number <- 1
 fig9 <- VMEVMS
 fig9$category <- paste(fig9$precaution,fig9$ref,sep="_")
 
+
 # subset fig9 in line with fishing footprint scenario 
-fig9 <- fig9[fig9$adjacent.cells > 0,]
+  fig9 <- fig9[!(fig9$adjacent.cells == 0 & fig9$ref==1),]
+
 
 #rm(list=setdiff(ls(), c("pathdir","pathdir_nogit","fig9" , "EcoReg")))
